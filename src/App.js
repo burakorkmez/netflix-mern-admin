82cb01868148ccc7d8330912c5c3b020cfd207b7
Burak Örkmez <burakorkmezz@gmail.com>
2022-01-16 23:34:28 +0100

    UserList added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Navbar from './components/navbar/Navbar';
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './pages/home/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import UserList from './pages/userList/UserList';
 
 function App() {
 	return (
 		<BrowserRouter>
-			<div className="App">
-				<Navbar />
-				<div className="container">
-					<Sidebar />
-					<Home />
-				</div>
+			<Navbar />
+			<div className="container">
+				<Sidebar />
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route exact path="/users">
+						<UserList />
+					</Route>
+				</Switch>
 			</div>
 		</BrowserRouter>
 	);
