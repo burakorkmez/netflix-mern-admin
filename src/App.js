12dd8ccb0feca3e3b769701786783e2988c26660
Burak Örkmez <burakorkmezz@gmail.com>
2022-01-16 21:56:11 +0100

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className="App">
-			<Navbar />
-		</div>
+		<BrowserRouter>
+			<div className="App">
+				<Navbar />
+				<div className="container">
+					<Sidebar />
+					<div className="other">other pages</div>
+				</div>
+			</div>
+		</BrowserRouter>
 	);
 }
 
