f7c141467083f38884e7f0fdbf8f2b6c61b7f35f
Burak Örkmez <burakorkmezz@gmail.com>
2022-01-17 01:08:33 +0100

    Product page added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
 import ProductList from './pages/productList/ProductList';
+import Product from './pages/product/Product';
 
 function App() {
 	return (
@@ -29,6 +30,9 @@ function App() {
 					</Route>
 					<Route path="/products">
 						<ProductList />
+					</Route>
+					<Route path="/product/:productId">
+						<Product />
 					</Route>
 				</Switch>
 			</div>
